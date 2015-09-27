d92c87faccfa45e8d50081fdd3ceb7cb8f53a784
Jonas Sjöberg <jomeganas@gmail.com>
2015-09-27 19:06:23 +0200

    Add empty classes.

---
+++ src/se/hig/oodp/lab/model/Rectangle.java
@@ -0,0 +1,20 @@
+/**
+ * DVG303 :: Objektorienterad design och programmering
+ * 860224 Jonas Sjöberg
+ * Högskolan i Gävle
+ * tel12jsg@student.hig.se
+ *
+ * Labb #1    Uppgift 1
+ */
+
+package se.hig.oodp.lab.model;
+
+/**
+ * @author  Jonas Sjöberg
+ *		    tel12jsg@student.hig.se
+ * @date	Sep 27, 2015
+ */
+public class Rectangle
+{
+
+}
