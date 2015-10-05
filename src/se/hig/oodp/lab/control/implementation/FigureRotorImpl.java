8c425fbbb7e692b308dfaa0047ab3370a859fc13
Jonas Sjöberg <jomeganas@gmail.com>
2015-10-05 07:53:11 +0200

    Add implementations of control interfaces.

---
+++ src/se/hig/oodp/lab/control/implementation/FigureRotorImpl.java
@@ -0,0 +1,25 @@
+/**
+ * DVG303 :: Objektorienterad design och programmering
+ * Laboration #2
+ * 860224 Jonas Sjöberg
+ * Högskolan i Gävle
+ * tel12jsg@student.hig.se
+ */
+
+package se.hig.oodp.lab.control.implementation;
+
+import se.hig.oodp.lab.control.FigureRotor;
+
+/**
+ * @author  Jonas Sjöberg
+ *		    tel12jsg@student.hig.se
+ * @date	Oct 5, 2015
+ */
+public class FigureRotorImpl implements FigureRotor
+{
+    @Override
+    public void rotateAll(double angle)
+    {
+
+    }
+}
---
+++ src/se/hig/oodp/lab/control/implementation/FigureScalorImpl.java
@@ -0,0 +1,25 @@
+/**
+ * DVG303 :: Objektorienterad design och programmering
+ * Laboration #2
+ * 860224 Jonas Sjöberg
+ * Högskolan i Gävle
+ * tel12jsg@student.hig.se
+ */
+
+package se.hig.oodp.lab.control.implementation;
+
+import se.hig.oodp.lab.control.FigureScalor;
+
+/**
+ * @author  Jonas Sjöberg
+ *		    tel12jsg@student.hig.se
+ * @date	Oct 5, 2015
+ */
+public class FigureScalorImpl implements FigureScalor
+{
+    @Override
+    public void scaleAll(double factor_x, double factor_y)
+    {
+
+    }
+}
